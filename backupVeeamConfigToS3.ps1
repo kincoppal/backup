a07d90f55d5ce0a8faa64d55e4122c7cbb29426e
James Davis <james.norman.davis@gmail.com>
2018-10-17 16:54:04 +1100

    Initial commit

---
+++ backupVeeamConfigToS3.ps1
@@ -0,0 +1,45 @@
+# Backup Veeam Config to AWS S3
+#
+# Description: Looks for synthetic full backup files and uploads
+# the most recent ones to bucket in S3. After a successful upload,
+# it will delete older backup files on S3 since archive data is
+# currently stored in tape library. Potential to use AWS Glacier
+# in the future
+# Created by James Davis 17/10/2018
+#
+
+Import-Module AWSPowerShell
+
+# Variable Constants
+$config = Get-Content -Raw -Path 'C:\Scripts\backup\config.json' | ConvertFrom-Json
+$source = 'F:\Backup\VeeamConfigBackup'
+[string]$emailBody = ""
+
+Initialize-AWSDefaultConfiguration -AccessKey $config.AKey -SecretKey $config.SKey -Region $config.region
+Set-Location $source
+
+$emailBody = $emailBody + $(Get-Date -Format o) + "`tStart Backup Job`n"
+
+$backupFolders = Get-ChildItem -Directory | Select-Object -Property FullName
+foreach($folder in $backupFolders) {
+    Set-Location $folder.FullName
+    $files = Get-ChildItem '*.bco' | Sort-Object CreationTime | Select-Object -Last 1 | Select-Object -Property FullName
+    foreach($file in $files){
+        # check if file already exists in S3
+        if(!(Get-S3Object -BucketName $config.bucket -Key $file.FullName)) {
+            try {
+                # upload the latest file
+                $emailBody = $emailBody + $(Get-Date -Format o) + "`tUploading file: " + $file.FullName + "`n"
+                Write-S3Object -BucketName $config.bucket -File $file.FullName -Key $file.FullName -CannedACLName private
+
+            } catch {
+                $emailBody = $emailBody + $(Get-Date -Format o) + "Error uploading file: $folder.FullName"
+            }
+        }
+    }
+}
+
+$emailBody = $emailBody + $(Get-Date -Format o) + "`tEnd Backup Job`n"
+$emailSubject = "Backup Veeam Config to AWS S3 Log - " + $(Get-Date -DisplayHint Date)
+
+Send-MailMessage -To $config.emailTo -From $config.emailFrom -Subject $emailSubject -SmtpServer $config.smtpServer -Body $emailBody
